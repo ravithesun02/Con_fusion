211305522e1049bd3eecbb8ae74df83fad6d7321
Ravi Kumar <ravithesun02@gmail.com>
2019-10-24 02:36:10 +0530

    grunt 1

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports=function(grunt){
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+    grunt.initConfig({
+        sass:{
+            dist:{
+                files:{
+                    'css/style.css':'css/style.scss'
+                }
+            }
+        },
+        watch:{
+            files:'css/*.scss',
+            tasks:['sass']
+        },
+        browserSync:{
+            dev:{
+                bsFiles:{
+                    src:['css/*.css',
+                '*.html',
+            'js/*.js']
+                },
+                options:{
+                    watchTask:true,
+                    server:{
+                        baseDir:'./'
+                    }
+                }
+            }
+        },
+
+        copy:{
+            html:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'./',
+                    src:['*.html'],
+                    dest:'dist'
+                }]
+            },
+            fonts:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'node_modules/font-awesome',
+                    src:['fonts/*.*'],
+                    dest:'dist'
+                }]
+            }
+        },
+
+        clean:{
+            build:{
+                src:['dist/']
+            }
+        },
+
+        imagemin:{
+            dynamic:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'./',
+                    src:['img/*.{png,gif,jpg}'],
+                    dest:'dist/'
+                }]
+            }
+        }
+    });
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin'
+    ]);
+};
